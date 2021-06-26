01d6c2debad1d65a3e621127371fef5b3e8c10f7
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-06-26 19:52:01 +0200

    Starting

---
+++ pipex.c
@@ -0,0 +1,66 @@
+#include "pipex.h"
+
+void	arg_error(void)
+{
+	ft_pustr_fd("Usage: ./pipex file1 \"cmd1 flags1\" \"cmd2 flags2\" file2", 2);
+	exit(0);
+}
+
+void	error_msg(void)
+{
+	char	*str;
+
+	str = strerror(errno);
+	write(2, str, ft_strlen(str));
+	write(2, "\n", 1);
+	exit(0);
+}
+
+/* Point is, the child process will trick the cmd1 into having 
+ * file1 as the STDIN (fd = 0), and write the output (fd = 1) on the
+ * write end of the pipe, which the father process will be able to
+ * read. This is achieved by duplicating the file1 fd on the
+ * STDIN, and the STDOUT on the write end of the pipe. This way 
+ * the parent process will read from the read end of the pipe
+ * whatever the first cmd outputted. */
+void	child(int p[2], char **argv, char **env)
+{
+	int		fd;
+	char	**flags;
+	char	*path;
+
+	close(p[0]); 					//We dont need the read end of the pipe in the child process.
+	fd = open(argv[1], O_RDONLY); 	// we open the archive given as an arg.
+	if (fd == -1)
+		error_msg();
+	dup2(fd, 0); 		//Now STDIN means fd, so if cm1 needs some input it will interpret whatever fd has.
+	dup2(p[1], 1); 		//Whatever execve does, it will be outputed to the write end of the pipe. 
+	close(p[1]); 		// We no longer need the fd from p[1], we have it on STDOUT.
+	path = find_exec_path(argv, env);
+	cmd = ft_split(argv[2], ' ');
+	if (execve(path, cmd, env) == -1)
+		error_msg();
+}
+
+/*Program that copies the < file1 cmd1 | cmd2 > file2 bash command.*/
+int main(int argc, char **argv, char **env)
+{
+	int		p[2];
+	t_pid	pid;
+
+	if (arg != 5)
+		arg_error();
+	if (pipe(p) == -1)
+		error_msg();
+	pid = fork();
+	if (pid == -1)
+		error_msg();
+	if (pid == 0)
+		child(p, argv, env);
+	else
+	{
+		//Parent process;
+
+	}
+	return (0);
+}
---
+++ pipex.h
@@ -0,0 +1,25 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <sys/wait.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "./libft/libft.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
