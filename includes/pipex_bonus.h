a0a43cc3fe946bee54e7c0f30ab213daa91873e0
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-07-22 20:12:00 +0200

    get next line utils added

--- includes/pipex_bonus.h
+++ includes/pipex_bonus.h
@@ -1,3 +1,5 @@
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 #include "pipex.h"
 
 /* Bonus functions */
@@ -21,3 +23,12 @@ void	dup_stdin_stdout_and_close(int new_in, int new_out);
 void	file_input_piped_output(t_pip *p, int pip[2]);
 void	piped_input_file_output(t_pip *p, pid_t pid_child, int pip[2]);
 void	piped_input_piped_output(t_pip *p, pid_t child, int old_pip[2]);
+
+// Gnl utils
+
+# define BUFFER_SIZE 100
+
+char	*ft_strjoin_n_free(char *s1, char *s2);
+int		get_next_line(int fd, char **line);
+
+#endif
