f183e218b89940b110302bbc67536b268790cb21
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-07-23 23:50:14 +0200

    added stuff

--- srcs/several_pipe_handling_bonus.c
+++ srcs/several_pipe_handling_bonus.c
@@ -2,25 +2,16 @@
 
 void	file_input_piped_output(t_pip *p, int pip[2])
 {
-	char	*path;
-	char	**cmd;
-
 	close(pip[0]);
 	p->fd = open(p->argv[1], O_RDONLY);
 	if (p->fd == -1)
 		error_msg_relative_to_file(p->argv[1]);
 	dup_stdin_stdout_and_close(p->fd, pip[1]);
-	cmd = ft_split(p->argv[2], ' ');
-	path = find_exec_path(cmd, p->env);
-	if (execve(path, cmd, p->env) == -1)
-	   error_msg();
+	call_execve(p);
 }
 
 void	piped_input_file_output(t_pip *p, pid_t pid_child, int pip[2])
 {
-	char	**cmd;
-	char	*path;
-
 	close(pip[1]);
 	p->pid = fork();
 	if (p->pid == 0)
@@ -28,10 +19,7 @@ void	piped_input_file_output(t_pip *p, pid_t pid_child, int pip[2])
 		waitpid(pid_child, 0, WNOHANG);
 		p->fd = open(p->argv[p->argc - 1], O_TRUNC | O_WRONLY | O_CREAT, 00644);
 		dup_stdin_stdout_and_close(pip[0], p->fd);
-		cmd = ft_split(p->argv[p->argc - 2], ' ');
-		path = find_exec_path(cmd, p->env);
-		if (execve(path, cmd, p->env) == -1)
-			error_msg();
+		call_execve(p);
 	}
 	else
 		wait_and_exit(p->pid);
@@ -40,8 +28,6 @@ void	piped_input_file_output(t_pip *p, pid_t pid_child, int pip[2])
 void	piped_input_piped_output(t_pip *p, pid_t pid_child, int old_pip[2])
 {
 	int		new_pip[2];
-	char	**cmd;
-	char	*path;
 
 	pipe(new_pip);
 	close(old_pip[1]);
@@ -50,10 +36,7 @@ void	piped_input_piped_output(t_pip *p, pid_t pid_child, int old_pip[2])
 	{
 		waitpid(pid_child, 0, WNOHANG);
 		dup_stdin_stdout_and_close(old_pip[0], new_pip[1]);
-		cmd = ft_split(p->argv[p->v_i], ' ');
-		path = find_exec_path(cmd, p->env);
-		if (execve(path, cmd, p->env) == -1)
-			error_msg();
+		call_execve(p);
 	}
 	else if (++(p->v_i) == p->argc - 2)
 		piped_input_file_output(p, p->pid, new_pip);
