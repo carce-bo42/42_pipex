7deed0b77c05c845fe6149ed3ed82914dddf19df
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-07-24 16:19:17 +0200

    Done I think

--- srcs/main_bonus.c
+++ srcs/main_bonus.c
@@ -7,9 +7,7 @@ static void	pipex_alpha(t_pip *p)
 	pipe(pip);
 	p->pid = fork();
 	if (p->pid == 0)
-	{	
 		file_input_piped_output(p, pip);
-	}
 	else
 	{
 		p->v_i = 3;
@@ -41,6 +39,7 @@ static void	pipex_beta(t_pip *p)
 int	main(int argc, char **argv, char **env)
 {
 	t_pip	p;
+	char	*hdoc;
 
 	p.argc = argc;
 	p.v_i = 2;
@@ -48,11 +47,12 @@ int	main(int argc, char **argv, char **env)
 	p.env = env;
 	p.fdi = dup(0);
 	p.fdo = dup(1);
+	hdoc = "here_doc";
 	if (argc < 5)
 		print_usage_and_exit();
-	else if (argc >= 5 && ft_strncmp(argv[1], "here_doc", 8))
+	else if (argc >= 5 && ft_strncmp(argv[1], hdoc, max_len(argv[1], hdoc)))
 		pipex_alpha(&p);
-	else if (argc >= 6 && !ft_strncmp(argv[1], "here_doc", 8))
+	else if (argc >= 6 && !ft_strncmp(argv[1], hdoc, max_len(argv[1], hdoc)))
 		pipex_beta(&p);
 	else
 		print_usage_and_exit();
